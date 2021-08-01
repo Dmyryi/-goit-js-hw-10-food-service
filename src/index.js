ff7bf6db4ce2707107852be7473897233f1cef05
Dmyryi <65827238+Dmyryi@users.noreply.github.com>
2021-08-01 19:04:13 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+<<<<<<< HEAD
+import cards from './teamplates/menu-cards.hbs';
+import menuItem from './menu.json';
+import './change-theme';
+import './styles.css';
+
+
+let menu = document.querySelector('.js-menu');
+
+function buildMenu(array) {
+    const markup = array.map(cards).join('');
+    menu.insertAdjacentHTML('beforeend', markup);
+}
+
+buildMenu(menuItem);
+=======
 
 import './css/styles.css';
 import './js/theme-change.js';
+>>>>>>> aca41e47a38be042b6adc8061c999f7cef9fc85f
